0b8ed70ef1b1ba3ff8fa5936aaf1fb45c741fd79
murilloves <murillo.vieiraesilva@gmail.com>
2018-09-29 16:52:39 -0300

    event speaker add improvs

--- src/pages/event-speaker-add/event-speaker-add.ts
+++ src/pages/event-speaker-add/event-speaker-add.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { EventModel } from '../../models/event.model';
 import { FormGroup, FormBuilder, Validators } from '../../../node_modules/@angular/forms';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -14,10 +15,13 @@ export class EventSpeakerAddPage {
   public event: EventModel;
   public form: FormGroup;
 
+  public myphoto: any;
+
   constructor(
     public navCtrl: NavController,
+    public navParams: NavParams,
+    private camera: Camera,
     private formBuilder: FormBuilder,
-    public navParams: NavParams
   ) {
     this.event = this.navParams.get('event');
     this.setClearForm();
@@ -34,6 +38,51 @@ export class EventSpeakerAddPage {
       resume: [''],
       image: [''],
     });
+  }
+  
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64,' + imageData;
+      document.getElementById('selecionarFoto').innerHTML = '';
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  uploadImageMobile() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64,' + imageData;
+      document.getElementById('selecionarFoto').innerHTML = '';
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  readImg($event) {
+    if (event.target && event.target['files'] && event.target['files'][0]) {
+      var reader = new FileReader();
+  
+      reader.onload = (event: ProgressEvent) => {
+        this.myphoto = (<FileReader>event.target).result;
+        document.getElementById('selecionarFoto').innerHTML = '';
+      }
+
+      reader.readAsDataURL(event.target['files'][0]);
+    }
   }
 
 }
